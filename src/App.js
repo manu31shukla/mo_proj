73045a0e7f7b92e17395a5950cbe40a1be0c2482
manu31shukla <manu31shukla@gmail.com>
2023-12-07 20:35:07 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import './App.css';
+import { Link } from 'react-router-dom';
 
 const App = () => {
   const [link, setLink] = useState('');
@@ -13,14 +15,15 @@ const App = () => {
   };
 
   const handlePlayClick = () => {
-    // Implement your play functionality here
+    // <Link to 'http://localhost:3000/play'>
     console.log('Playing:', link, 'with ID:', id);
   };
+
 
   return (
     <div style={styles.container}>
       <h1 style={styles.title}>
-        First join the discord bot to continue with the service of playing music
+        First join the Discord bot to continue with the service of playing music
       </h1>
       <div style={styles.column}>
         <label style={styles.label}>Enter Link:</label>
@@ -53,11 +56,18 @@ const styles = {
     flexDirection: 'column',
     alignItems: 'center',
     padding: '20px',
+    backgroundColor: '#f0f0f0',
+    borderRadius: '10px',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+    maxWidth: '400px',
+    margin: 'auto',
+    marginTop: '50px',
   },
   title: {
     fontSize: '24px',
     marginBottom: '20px',
     textAlign: 'center',
+    color: '#333',
   },
   column: {
     display: 'flex',
@@ -66,19 +76,23 @@ const styles = {
   },
   label: {
     marginBottom: '5px',
+    color: '#555',
   },
   input: {
-    padding: '8px',
+    padding: '10px',
     fontSize: '16px',
+    border: '1px solid #ccc',
+    borderRadius: '5px',
   },
   button: {
-    padding: '10px 20px',
+    padding: '12px 20px',
     fontSize: '18px',
     backgroundColor: '#3498db',
     color: '#fff',
     cursor: 'pointer',
     border: 'none',
     borderRadius: '5px',
+    transition: 'background-color 0.3s ease',
   },
 };
 
